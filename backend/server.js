9ef126ac45be69050ddff441ed20564088f12126
SwapniL1563 <choratswapnil15@gmail.com>
2025-07-05 01:34:59 +0530

    fixed JWT refresh token flow logic

--- backend/server.js
+++ backend/server.js
@@ -15,13 +15,16 @@ dotenv.config();
 const app = express();
 
 // middleware
-app.use(cors({
-  origin: 'https://mednutri-frontend.vercel.app',
+const corsOptions = {
+  origin: "https://mednutri-frontend.vercel.app",
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
